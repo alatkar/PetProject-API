9b7207e2f210d98114d68637ed7514031e0073ac
Atul Alatkar <alatkar@yahoo.com>
2017-06-18 11:35:49 -0700

    Initial working commit

---
+++ src/PetProject-API/Controllers/FeedController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetProject_API
+{
+    [Route("api/[controller]")]
+    public class FeedController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Feed> Get()
+        {
+            //var ctx = new ApplicationDbContext(" ");
+            //ctx.
+
+            return new Feed[] {
+                new Feed() { message = "First Fedd", type = Feed.FeedType.POST },
+                new Feed() { message = "Second Fedd", type = Feed.FeedType.POST },
+                new Feed() { message = "Third Fedd", type = Feed.FeedType.POST },
+                new Feed() { message = "Fourth Fedd", type = Feed.FeedType.POST },
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public class Feed
+        {
+            public enum FeedType
+            {
+                POST = 0
+            };
+
+            public string message { get; set; }
+            public FeedType type { get; set; }
+        }
+    }
+}
---
+++ src/PetProject-API/Controllers/RecommendController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetProject_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecommendController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Recommendation> Get()
+        {
+            return new Recommendation[] {
+                new Recommendation() { message = "Redmond Kennel Services", type = Recommendation.RecommendationType.PRODUCT, linkId = "1234" },
+                new Recommendation() { message = "Reliable Dog Trainer", type = Recommendation.RecommendationType.PRODUCT, linkId = "2234" },
+                new Recommendation() { message = "Jump Start Dog Walker", type = Recommendation.RecommendationType.PRODUCT , linkId = "3234"},
+                new Recommendation() { message = "Mayflower dog groomer", type = Recommendation.RecommendationType.PRODUCT , linkId = "4234"}
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public class Recommendation
+        {
+            public enum RecommendationType
+            {
+                PRODUCT = 0,
+                SERVICE,
+            };
+
+            public string message { get; set; }
+            public RecommendationType type { get; set; }
+            public string linkId { get; set; }
+        }
+    }
+}
